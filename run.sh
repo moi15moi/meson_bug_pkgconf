748d581240de4c581b1ed23142868a8f7fcfcd3c
moi15moi <moi15moismokerlolilol@gmail.com>
2026-06-06 20:14:56 -0400

    Rename project_1 folder to project_😋_1 to provoke the bug

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
-cd project_1
+cd project_😋_1
 rm -rf builddir
 meson setup builddir -Dlibdir=lib
 meson compile -C builddir
 meson install -C builddir --destdir install_dir
 cd ..
 unset PKG_CONFIG_PATH
-export PKG_CONFIG_LIBDIR="$(pwd)/project_1/builddir/install_dir/usr/local/lib/pkgconfig"
-export PKG_CONFIG_SYSROOT_DIR="$(pwd)/project_1/builddir/install_dir"
+export PKG_CONFIG_LIBDIR="$(pwd)/project_😋_1/builddir/install_dir/usr/local/lib/pkgconfig"
+export PKG_CONFIG_SYSROOT_DIR="$(pwd)/project_😋_1/builddir/install_dir"
 cd project_2
 rm -rf builddir
 meson setup builddir
